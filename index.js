f20c3de2e9f25bcfb6f420245559c753a3a5ebf5
ashwini.sapre <ashwini.sapre@knoxpo.com>
2021-11-10 16:24:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var parser = require('fast-xml-parser');
+var fs = require('fs');
+
+
+var data = fs.readFileSync("./strings-hi.xml", "utf8");
+
+let text = '<resources><string translatable="false" name="abc">Hello</string><string name="def">World</string></resources>';
+try {
+    var jsonObj = parser.parse(data, {ignoreAttributes: false, stopNodes: ["string"]}, true);
+
+    jsonObj.resources.string.forEach(stringElement => {
+        console.log(stringElement);
+        var item = `"${stringElement['@_name']}","${stringElement['#text'].trim()}"`
+        fs.appendFileSync('./output.csv', item+'\n');
+    });
+
+    console.log("DONE");
+} catch (error) {
+    console.log(error.message)
+}
+
